d860f7bce026c105346ab86b5b8e0b60f2ebfc8a
Peter Rogers <p.rogers@gold.ac.uk>
2024-01-24 11:25:10 +0000

    Initial Commit

---
+++ swiftSerialConnection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swiftSerialConnection
+//
+//  Created by Peter Rogers on 24/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
